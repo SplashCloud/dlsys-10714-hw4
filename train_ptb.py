07f84c9cd606978d9ed76cc21dca292870c1c26d
Hanchao <2731293166@qq.com>
2025-02-07 13:02:47 +0800

    update train result

--- train_ptb.py
+++ train_ptb.py
@@ -13,3 +13,5 @@ train_acc, train_loss = train_ptb(model, train_data, seq_len=1, n_epochs=1, devi
 print(f'train_acc={train_acc}, train_loss={train_loss}')
 test_acc, test_loss = evaluate_ptb(model, train_data, seq_len=40, device=device)
 print(f'test_acc={test_acc}, train_loss={train_loss}')
+# train_acc=0.04267539631994768, train_loss=0.44389123265362873
+# test_acc=0.035492174851011055, train_loss=0.44389123265362873
--- train_transformer.py
+++ train_transformer.py
@@ -5,11 +5,14 @@ import needle as ndl
 from models import LanguageModel
 from simple_ml import train_ptb, evaluate_ptb
 
-device = ndl.cuda()
+device = ndl.cpu()
 corpus = ndl.data.Corpus("data/ptb")
-train_data = ndl.data.batchify(corpus.train, batch_size=16, device=device, dtype="float32")
-model = LanguageModel(30, len(corpus.dictionary), hidden_size=10, num_layers=2, seq_model='transformer', device=device)
-train_acc, train_loss = train_ptb(model, train_data, seq_len=1, n_epochs=1, device=device)
+train_data = ndl.data.batchify(corpus.train, batch_size=256, device=device, dtype="float32")
+model = LanguageModel(20, len(corpus.dictionary), hidden_size=32, num_layers=1, seq_model='transformer', seq_len=20, device=device)
+train_acc, train_loss = train_ptb(model, train_data, seq_len=20, n_epochs=10, device=device, lr=0.003, optimizer=ndl.optim.Adam)
 print(f'train_acc={train_acc}, train_loss={train_loss}')
-test_acc, test_loss = evaluate_ptb(model, train_data, seq_len=40, device=device)
+test_acc, test_loss = evaluate_ptb(model, train_data, seq_len=20, device=device)
 print(f'test_acc={test_acc}, train_loss={train_loss}')
+# max_lines = 1000
+# train_acc=0.2371124267578125, train_loss=0.0008183120240573772
+# test_acc=0.2341888427734375, train_loss=0.0008183120240573772
